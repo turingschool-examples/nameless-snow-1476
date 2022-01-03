1e1ae466bbce005a96b1aba49252b48117545e63
Paul Leonard <paulpaulleonard@gmail.com>
2022-01-03 12:34:36 -0500

    fixed relationships error.  writing test for mechanics index

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Mechanic do
   describe 'relationships' do
-    xit { should have_many(:rides).through(:ride_mechanics) }
-      #I cannot for the life of me figure out why this wont pass
+    it { should have_many(:rides).through(:ride_mechanics) }
   end
 end
--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe Ride do
   describe 'relationships' do
     it { should belong_to(:amusement_park) }
-    xit { should have_many(:mechanics).through(:ride_mechanics) }
-    #I cannot for the life of me figure out why this wont pass
+    it { should have_many(:mechanics).through(:ride_mechanics) }
   end
 end
