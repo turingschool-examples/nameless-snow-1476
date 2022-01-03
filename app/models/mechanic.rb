99dd3696d1e352e95c8d2e2928f93d370a03288f
Robin Dittrich <robinodittrich@gmail.com>
2022-01-03 09:15:47 -0800

    Create method for yoe_average.

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -3,4 +3,8 @@ class Mechanic < ApplicationRecord
   has_many :mechanic_rides, dependent: :destroy
 
   validates_presence_of :name, :years_experience
+
+  def self.yoe_average
+    average(:years_experience)
+  end
 end
