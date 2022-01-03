e2dc73f7733a99a32ed0acd6033c1d1d5e02ae88
Parker <88950699+ParkerLockhart@users.noreply.github.com>
2022-01-03 11:07:25 -0600

    Average_experience class method

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,4 +1,8 @@
 class Mechanic < ApplicationRecord
   has_many :ride_mechanics
   has_many :rides, through: :ride_mechanics
-end 
+
+  def self.average_experience
+    average(:years_experience).to_i
+  end
+end
