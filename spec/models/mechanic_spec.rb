0d6507fd10ec62da199b9334b26829fc04e8365b
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-01-03 09:40:14 -0700

    Filled in test for ::average_exp

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -6,8 +6,13 @@ RSpec.describe Mechanic do
     it { should have_many(:rides).through(:ride_mechanics) }
   end
 
+  let!(:teddy) {Mechanic.create!(name: "Teddy Handyman", years_of_experience: 23)}
+  let!(:bob) {Mechanic.create!(name: "Bob Belcher", years_of_experience: 3)}
+  let!(:jim) {Mechanic.create!(name: "Jim Redman", years_of_experience: 35)}
+
   describe '::average_exp' do
     it 'calculates the average years of experience for all mechanics' do
+      expect(Mechanic.average_exp).to eq(20.33)
     end
-  end 
+  end
 end
