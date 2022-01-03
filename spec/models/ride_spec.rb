6856caf9a4e66e21a33134e5b6f295121a989876
Tim Roeder <timothyjroeder@gmail.com>
2022-01-03 09:38:50 -0700

    Feat: add Ride validations and tests

--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -6,4 +6,9 @@ RSpec.describe Ride do
     it { should have_many(:ride_mechanics) }
     it { should have_many(:mechanics).through(:ride_mechanics) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :thrill_rating }
+  end
 end
