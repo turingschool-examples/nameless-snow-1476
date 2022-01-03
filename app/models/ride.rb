b92839386d80843b1787e1745f56e05c4475513a
Brad Breiten <jonathan@breiten.us>
2022-01-03 10:43:21 -0700

    Feature: mechanic show pages displays name, experience, and only open rides the mechanic is working on

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -3,4 +3,8 @@ class Ride < ApplicationRecord
 
   has_many :mechanic_rides
   has_many :mechanics, through: :mechanic_rides
+
+  def self.most_to_least_thrilling
+    order(thrill_rating: :desc)
+  end
 end
