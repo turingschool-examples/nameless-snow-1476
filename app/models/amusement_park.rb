40b6e4dd9a37d871e7ecf4994dcca491aa958ae2
Leland Curtis <lelandcurtis88@gmail.com>
2022-01-03 13:51:13 -0500

    Add validations.

--- app/models/amusement_park.rb
+++ app/models/amusement_park.rb
@@ -1,3 +1,6 @@
 class AmusementPark < ApplicationRecord
   has_many :rides
+
+  validates :name, presence: true
+  validates :admission_cost, presence: true, numericality: true
 end
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -3,6 +3,11 @@ class Ride < ApplicationRecord
   has_many :ride_mechanics
   has_many :mechanics, through: :ride_mechanics
 
+  validates :name, presence: true
+  validates :thrill_rating, presence: true, numericality: true
+  validates :open, presence: true, inclusion: {in: [true, false]}
+
+
   def self.by_thrill
     order(thrill_rating: :desc)
   end
