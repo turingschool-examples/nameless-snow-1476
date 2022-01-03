9104aee9a54fbcf77eb46d2b198fa5cbbe7e7086
Kevin Gloss <kgloss89@gmail.com>
2022-01-03 11:44:29 -0700

    Test average thrill rating for rides

--- spec/models/amusement_park_spec.rb
+++ spec/models/amusement_park_spec.rb
@@ -10,11 +10,15 @@ RSpec.describe AmusementPark do
 
     @hurler = @six_flags.rides.create!(name: 'The Hurler', thrill_rating: 5, open: true)
     @scrambler = @six_flags.rides.create!(name: 'The Scrambler', thrill_rating: 7, open: true)
-    @wheel = @six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 7, open: false)
+    @wheel = @six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 3, open: false)
     @mind = @six_flags.rides.create!(name: 'Mind Eraser', thrill_rating: 9, open: true)
   end
 
   it 'can sort rides alphabetically' do
     expect(@six_flags.rides_alpha).to eq([@wheel, @mind, @hurler, @scrambler])
+  end
+
+  it 'can calculate the rides average thrill rating' do
+    expect(@six_flags.average_thrill).to eq(6)
   end
 end
