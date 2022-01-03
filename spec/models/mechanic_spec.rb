fb92eb91a94836f5f6a656f55d0214d444d207fd
Leland Curtis <lelandcurtis88@gmail.com>
2022-01-03 12:53:39 -0500

    Add test for open_rides_by_thrill.

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -38,4 +38,20 @@ RSpec.describe Mechanic, type: :model do
       end
     end
   end
+
+  describe 'open_rides_by_thrill' do
+    it "returns open rides by thrill reating in desending order" do
+      mech = create(:mechanic)
+      ride_1 = create(:ride, thrill_rating: 10)
+      ride_2 = create(:ride, thrill_rating: 15)
+      ride_3 = create(:ride, open: false)
+      ride_4 = create(:ride, thrill_rating: 5)
+      ride_mechanic_1 = create(:ride_mechanic, ride: ride_1, mechanic: mech)
+      ride_mechanic_2 = create(:ride_mechanic, ride: ride_2, mechanic: mech)
+      ride_mechanic_3 = create(:ride_mechanic, ride: ride_3, mechanic: mech)
+      ride_mechanic_4 = create(:ride_mechanic, ride: ride_4, mechanic: mech)
+
+      expect(Mechanic.open_rides_by_thrill).to eq([ride_2, ride_1, ride_4])
+    end
+  end
 end
