76b165858e9283d7f4374eaeb002d5eb7b9807b4
Leland Curtis <lelandcurtis88@gmail.com>
2022-01-03 13:14:08 -0500

    Write test for new ride form on mech show page.

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -52,4 +52,26 @@ RSpec.describe 'mechanic show page' do
       expect(ride_1.name).to appear_before(ride_3.name)
     end
   end
+
+  it "has a form to add an existing ride to their workload" do
+    mech = create(:mechanic)
+    ride_1 = create(:ride, thrill_rating: 10)
+    ride_2 = create(:ride, thrill_rating: 15)
+    ride_3 = create(:ride, thrill_rating: 4)
+    ride_4 = create(:ride, thrill_rating: 100)
+    ride_mechanic_1 = create(:ride_mechanic, ride: ride_1, mechanic: mech)
+    ride_mechanic_2 = create(:ride_mechanic, ride: ride_2, mechanic: mech)
+    ride_mechanic_3 = create(:ride_mechanic, ride: ride_3, mechanic: mech)
+
+    visit "/mechanics/#{mech.id}"
+    expect(page).to_not have_content(ride_4.name)
+
+    within 'new_ride' do
+      fill_in 'Ride ID', with: ride_4.id
+      click_button "Submit"
+    end
+
+    expect(current_path).to eq("/mechanics/#{mech.id}")
+    expect(page).to have_content(ride_4.name)
+  end
 end
