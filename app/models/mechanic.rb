f42e0212cc7ace6ce98a70ea1cf50db31c1b8ed4
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-01-03 09:41:29 -0700

    ::average_exp passing tests

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,4 +1,8 @@
 class Mechanic < ApplicationRecord
   has_many :ride_mechanics
   has_many :rides, through: :ride_mechanics
+
+  def self.average_exp
+    average(:years_of_experience).round(2)
+  end
 end
