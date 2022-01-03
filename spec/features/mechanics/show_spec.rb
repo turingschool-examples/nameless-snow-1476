1311a8aa48694060c800e6dc3f44301a63017c76
Devin Pile <devinpile@gmail.com>
2022-01-03 11:05:45 -0700

    Add tests for add ride to mechanic

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 describe 'Mechanic Show' do
   before do
-    park = AmusementPark.create(name: 'Hug Land', admission_cost: 1000)
+    @park = AmusementPark.create(name: 'Hug Land', admission_cost: 1000)
     @mechanic1 = Mechanic.create(name: 'Kara Smith', years_experience: 11)
     @mechanic2 = Mechanic.create(name: 'Sam Jones', years_experience: 7)
-    @ride1 = Ride.create(name: 'Ferris Wheel', thrill_rating: 3, open: true, amusement_park: park)
-    @ride2 = Ride.create(name: 'Loop-D-Loop', thrill_rating: 10, open: true, amusement_park: park)
-    @ride3 = Ride.create(name: 'Spin-E-Thing', thrill_rating: 6, open: false, amusement_park: park)
-    @ride4 = Ride.create(name: 'Round-A-Bout', thrill_rating: 6, open: false, amusement_park: park)
+    @ride1 = Ride.create(name: 'Ferris Wheel', thrill_rating: 3, open: true, amusement_park: @park)
+    @ride2 = Ride.create(name: 'Loop-D-Loop', thrill_rating: 10, open: true, amusement_park: @park)
+    @ride3 = Ride.create(name: 'Spin-E-Thing', thrill_rating: 6, open: false, amusement_park: @park)
+    @ride4 = Ride.create(name: 'Round-A-Bout', thrill_rating: 6, open: false, amusement_park: @park)
 
     RideMechanic.create!(mechanic: @mechanic1, ride: @ride1)
     RideMechanic.create!(mechanic: @mechanic1, ride: @ride2)
@@ -28,6 +28,18 @@ describe 'Mechanic Show' do
     it 'open rides which mechanic is working on, in thrill rate desc order' do
       expect(page).to have_content("Current rides they're working on:")
       expect(@ride2.name).to appear_before(@ride1.name)
+    end
+
+    describe 'add a ride to a mechanic' do
+      it 'form to add existing ride to mechanic workload' do
+        expect(page).to_not have_content(@ride4.name)
+
+        fill_in :ride_id, with: @ride4.id
+        click_button "Add Ride"
+
+        expect(page).to have_content(@ride4.name)
+        expect(current_path).to eq("/mechanics/#{@mechanic1.id}")
+      end
     end
   end
 end
