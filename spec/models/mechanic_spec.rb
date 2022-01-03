209afa0d768ed0553d577b7cadb9b810f8df4bd6
Kelly Anderson <kean9528@colorado.edu>
2022-01-03 09:51:34 -0700

    finish functionality for user story 1

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -1,8 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Mechanic do
+
+  before do
+    @frank = Mechanic.create!(name: "Frank", years_experience: 5)
+    @kara = Mechanic.create!(name: "Kara", years_experience: 15)
+  end
+
   describe 'relationships' do
     it { should have_many(:rides) }
     it { should have_many(:ride_mechanics) }
+  end
+
+  describe 'methods' do
+    it '#average_years' do
+      expect(Mechanic.average_years).to eq(10)
+    end
   end
 end
