ec7668e0724537d588892d0befc17fc2ef4f2bbc
Parker <88950699+ParkerLockhart@users.noreply.github.com>
2022-01-03 12:54:41 -0600

    Add instance methods

--- app/models/amusement_park.rb
+++ app/models/amusement_park.rb
@@ -1,3 +1,11 @@
 class AmusementPark < ApplicationRecord
   has_many :rides
+
+  def alpha_rides
+    rides.order(name: :asc)
+  end
+
+  def avg_ride_thrill
+    rides.average(:thrill_rating).to_i
+  end
 end
