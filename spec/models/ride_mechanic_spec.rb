1f928c4d4c8ab1bd1e1ca727a383a78b98f7612f
Paul Leonard <paulpaulleonard@gmail.com>
2022-01-03 11:46:10 -0500

    Create tests for mechanic and ride mechanic models

---
+++ spec/models/ride_mechanic_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+describe RideMechanic, type: :model do
+  describe "relationships" do
+    it { should belong_to(:rides) }
+    it { should belong_to(:mechanics)}
+  end
+end
