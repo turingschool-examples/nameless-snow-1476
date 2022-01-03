422f3488971f15f8844faa4133a66bea95147574
Tim Roeder <timothyjroeder@gmail.com>
2022-01-03 11:58:33 -0700

    Feat: add #rides_alphabetical and #avg_thrill

--- app/models/amusement_park.rb
+++ app/models/amusement_park.rb
@@ -1,3 +1,14 @@
 class AmusementPark < ApplicationRecord
   has_many :rides
+
+  validates_presence_of :name, :admission_cost
+
+  # Instance methods
+  def rides_alphabetical
+    rides.order(:name)
+  end
+
+  def avg_thrill
+    rides.average(:thrill_rating).round(1)
+  end
 end
