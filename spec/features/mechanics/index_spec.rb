ef67a09116bda149fc346f0e94ea32a5555f4099
Paul Leonard <paulpaulleonard@gmail.com>
2022-01-03 12:46:38 -0500

    finish tests for story 1

--- spec/features/mechanics/index_spec.rb
+++ spec/features/mechanics/index_spec.rb
@@ -1,19 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe 'Mechanics Index' do
-  xit "has a header" do
+  it "has a header" do
     visit '/mechanics'
 
     expect(page).to have_content("All Mechanics")
   end
 
-  xit "lists all mechanics by name and has their years of experience" do
+  it "lists all mechanics by name and has their years of experience" do
     mech1 = Mechanic.create(name: "Paul", years_experience: 2)
     mech2 = Mechanic.create(name: "paulie", years_experience: 4)
 
     visit '/mechanics'
 
+    expect(page).to have_content("Paul")
+    expect(page).to have_content(2)
+    expect(page).to have_content("paulie")
+    expect(page).to have_content(4)
+    expect(page).to_not have_content("Stacy")
+    expect(page).to_not have_content(70)
+  end
 
+  it "shows the average years of experience across all mechanics" do
+    mech1 = Mechanic.create(name: "Paul", years_experience: 2)
+    mech2 = Mechanic.create(name: "paulie", years_experience: 4)
+
+    visit '/mechanics'
+
+    expect(page).to have_content("Average years of experience: 3")
   end
 
 end
