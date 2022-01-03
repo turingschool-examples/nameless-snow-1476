556f573557fa94b72937866ee1c8710ea1eb4b7b
Leland Curtis <lelandcurtis88@gmail.com>
2022-01-03 11:46:24 -0500

    Write model test for mechanic avg_yr_experience.

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Mechanic, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'class methods' do
+    describe 'avg_years_experience' do
+      it 'returns the avg_years_experience of all mechanics' do
+        mech_1 = create(:mechanic, years_experience: 10)
+        mech_1 = create(:mechanic, years_experience: 15)
+        mech_1 = create(:mechanic, years_experience: 13)
+
+        expect(Mechanic.avg_years_experience).to eq(12.67)
+      end
+    end
+  end
 end
