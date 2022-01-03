277e6d5afc082e666b31808e49de77c47c344bcd
Joel Grant <joelmgrant@me.com>
2022-01-03 11:06:21 -0700

    Feat: Create mechanic show page

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -3,4 +3,9 @@ def index
   @mechanics = Mechanic.all
   @average_experience = Mechanic.experience
 end
+
+def show
+  @mechanic = Mechanic.find(params[:id])
+  @rides = @mechanic.open_rides 
+end
 end
