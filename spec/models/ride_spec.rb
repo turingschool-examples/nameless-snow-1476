d3a7b3c1f50c5e649288cc005e9fa3a761306ac6
Paul Leonard <paulpaulleonard@gmail.com>
2022-01-03 12:16:36 -0500

    add notation to failing tests

--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe Ride do
   describe 'relationships' do
     it { should belong_to(:amusement_park) }
     xit { should have_many(:mechanics).through(:ride_mechanics) }
+    #I cannot for the life of me figure out why this wont pass
   end
 end
