fc280bbdfecbd04e862de026c4ceed7808df870b
Kevin Gloss <kgloss89@gmail.com>
2022-01-03 10:29:46 -0700

    Add class methods open? and desc_thrill

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -2,4 +2,12 @@ class Ride < ApplicationRecord
   belongs_to :amusement_park
   has_many :mechanic_rides
   has_many :mechanics, through: :mechanic_rides
+
+  def self.open?
+    where(open: true)
+  end
+
+  def self.desc_thrill
+    order(thrill_rating: :desc)
+  end
 end
