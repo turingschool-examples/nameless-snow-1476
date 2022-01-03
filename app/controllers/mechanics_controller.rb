9c3f7ae29d3b94e2d811feb8416d641026140bec
Parker <88950699+ParkerLockhart@users.noreply.github.com>
2022-01-03 12:23:12 -0600

    Add form action

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -5,5 +5,11 @@ class MechanicsController < ApplicationController
 
   def show
     @mechanic = Mechanic.find(params[:id])
-  end 
+
+    if params[:ride_id].present?
+      @ride = Ride.find(params[:ride_id])
+      RideMechanic.create!(mechanic: @mechanic, ride: @ride)
+      redirect_to "/mechanics/#{@mechanic.id}"
+    end
+  end
 end
