3dfdf509cafe1bc8695b39078114d40eb2b94e32
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-01-03 14:07:50 -0500

    Add edit method that adds existing ride to mechanic workload

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -6,4 +6,13 @@ class MechanicsController < ApplicationController
    def show
      @mechanic = Mechanic.find(params[:id])
    end
+
+   def edit
+     # require "pry"; binding.pry
+     find = Ride.where(id: params[:ride_id])
+     mechanic = Mechanic.find(params[:id])
+     mechanic.rides << find
+     redirect_to "/mechanics/#{mechanic.id}"
+
+ end
 end
