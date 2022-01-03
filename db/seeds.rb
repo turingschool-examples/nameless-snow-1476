ec3bf9a7f60ebcb241cb0f8d204aab246d6168b4
Jessica Grazulis <jlgrazulis@gmail.com>
2022-01-03 10:35:51 -0700

    Fix many to many relationship

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,9 @@ hurler = six_flags.rides.create!(name: 'The Scrambler', thrill_rating: 4, open: 
 hurler = six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 7, open: false)
 
 jaws = universal.rides.create!(name: 'Jaws', thrill_rating: 5, open: true)
+
+sally = Mechanic.create!(name: "Sally Field", years_experience: 15)
+martin = Mechanic.create!(name: "Martin Sheen", years_experience: 20)
+andrew = Mechanic.create!(name: "Andrew Garfield", years_experience: 11)
+emma = Mechanic.create!(name: "Emma Stone", years_experience: 12)
+
