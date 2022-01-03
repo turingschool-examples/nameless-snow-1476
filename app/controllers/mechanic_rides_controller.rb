1d557a516314b0423a7a32018a5330c353bc5d75
Kevin Gloss <kgloss89@gmail.com>
2022-01-03 11:05:33 -0700

    Add redirect to create method

--- app/controllers/mechanic_rides_controller.rb
+++ app/controllers/mechanic_rides_controller.rb
@@ -3,5 +3,7 @@ class MechanicRidesController < ApplicationController
     mechanic = Mechanic.find(params[:id])
     ride = Ride.find(params[:ride_id])
     MechanicRide.create(mechanic: mechanic, ride: ride)
+
+    redirect_to "/mechanics/#{mechanic.id}"
   end
 end
