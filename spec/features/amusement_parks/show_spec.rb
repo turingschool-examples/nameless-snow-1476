e222fb4053939a9c95c8c704874f926251c776bf
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-01-03 11:10:04 -0700

    Rewrite test, passing second test

--- spec/features/amusement_parks/show_spec.rb
+++ spec/features/amusement_parks/show_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 RSpec.describe 'amusement_park show page' do
   let!(:wonder_wharf) {AmusementPark.create!(name: "Wonder Wharf", admission_cost: 3)}
 
-  let(:merry_go_round) {Ride.create!(name: "Happy Whale", thrill_rating: 3, open: true, amusement_park: wonder_wharf)}
-  let(:coaster) {Ride.create!(name: "Whirly Whale", thrill_rating: 8, open: true, amusement_park: wonder_wharf)}
-  let(:bumper_cars) {Ride.create!(name: "Bumpy Whale", thrill_rating: 6, open: true, amusement_park: wonder_wharf)}
-  let(:haunted_house) {Ride.create!(name: "Ghost Whale", thrill_rating: 7, open: false, amusement_park: wonder_wharf)}
-  let(:skating_rink) {Ride.create!(name: "Frozen Whale", thrill_rating: 5, open: false, amusement_park: wonder_wharf)}
-
   before :each do
+    @merry_go_round = wonder_wharf.rides.create!(name: "Happy Whale", thrill_rating: 3, open: true)
+    @coaster = wonder_wharf.rides.create!(name: "Whirly Whale", thrill_rating: 8, open: true)
+    @bumper_cars = wonder_wharf.rides.create!(name: "Bumpy Whale", thrill_rating: 6, open: true)
+    @haunted_house = wonder_wharf.rides.create!(name: "Ghost Whale", thrill_rating: 7, open: false)
+    @skating_rink = wonder_wharf.rides.create!(name: "Frozen Whale", thrill_rating: 5, open: false)
+
     visit "/amusement_park/#{wonder_wharf.id}"
   end
 
@@ -21,10 +21,10 @@ RSpec.describe 'amusement_park show page' do
 
   it 'displays the names of all rides in alphabetical order' do
 
-    expect(bumper_cars.name).to appear_before(skating_rink.name)
-    expect(skating_rink.name).to appear_before(haunted_house.name)
-    expect(haunted_house.name).to appear_before(merry_go_round.name)
-    expect(merry_go_round.name).to appear_before(coaster.name)
+    expect(@bumper_cars.name).to appear_before(@skating_rink.name)
+    expect(@skating_rink.name).to appear_before(@haunted_house.name)
+    expect(@haunted_house.name).to appear_before(@merry_go_round.name)
+    expect(@merry_go_round.name).to appear_before(@coaster.name)
   end
 
   xit 'displays an average thrill rating for all rides' do
