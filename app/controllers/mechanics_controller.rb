758a30f0277db1fc4a6ec68bf500629c237f45f1
Tim Roeder <timothyjroeder@gmail.com>
2022-01-03 11:27:11 -0700

    Feat: add #update, #set_mechanic, and #find_ride actions and callback

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -1,9 +1,26 @@
 class MechanicsController < ApplicationController
+  before_action :set_mechanic, only: [:show, :update]
+
   def index
     @mechanics = Mechanic.all
   end
 
   def show
+  end
+
+  def update
+    new_ride = Ride.find(find_ride[:mechanic][:ride_id])
+    @mechanic.rides << new_ride
+    redirect_to mechanic_path(@mechanic)
+  end
+
+  private
+
+  def set_mechanic
     @mechanic = Mechanic.find(params[:id])
+  end
+
+  def find_ride
+    params.permit(:id, mechanic: [:ride_id])
   end
 end
