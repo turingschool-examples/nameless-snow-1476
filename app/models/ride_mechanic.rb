b0f466a535b2736c143b908212ac4952e2bf790b
Leland Curtis <lelandcurtis88@gmail.com>
2022-01-03 13:27:05 -0500

    Add new method to ride mechanic. Update form with proerp route and query to add mechanic id.

--- app/models/ride_mechanic.rb
+++ app/models/ride_mechanic.rb
@@ -1,4 +1,9 @@
 class RideMechanic < ApplicationRecord
   belongs_to :ride
   belongs_to :mechanic
+
+  def new
+    RideMechanic.create(ride_id: params[:ride_id], mechanic_id: params[:mechanic_id])
+    redirect_to "mechanics/#{params[:mechanic_id]}"
+  end
 end
