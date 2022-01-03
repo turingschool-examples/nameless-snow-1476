5076c108212d025be81283a54abef2058c1e660c
Tim Roeder <timothyjroeder@gmail.com>
2022-01-03 10:07:45 -0700

    Feat: add Mechanic::avg_years

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -3,4 +3,9 @@ class Mechanic < ApplicationRecord
   has_many :rides, through: :ride_mechanics
 
   validates_presence_of :name, :years_experience
+
+  # Class Methods
+  def self.avg_years
+    average(:years_experience).round(2)
+  end
 end
