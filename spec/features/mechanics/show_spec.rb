5048bfb2584f296d829e319de02c4dd03f0270c2
Dylan Harper <dylanharper4@icloud.com>
2022-01-03 09:54:34 -0800

    Finish us2

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -10,11 +10,13 @@ RSpec.describe 'mechanic show page' do
 
     @hurler = @six_flags.rides.create!(name: 'The Hurler', thrill_rating: 7, open: true)
     @scrambler = @six_flags.rides.create!(name: 'The Scrambler', thrill_rating: 4, open: false)
-    @ferris = @six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 7, open: true)
+    @ferris = @six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 8, open: true)
+    @tumbler = @six_flags.rides.create!(name: 'Tumbler', thrill_rating: 5, open: true)
 
     @ride_mechanic_1 = RideMechanic.create!(ride_id: @hurler.id, mechanic_id: @mechanic_1.id)
     @ride_mechanic_2 = RideMechanic.create!(ride_id: @scrambler.id, mechanic_id: @mechanic_1.id)
     @ride_mechanic_3 = RideMechanic.create!(ride_id: @ferris.id, mechanic_id: @mechanic_1.id)
+    @ride_mechanic_3 = RideMechanic.create!(ride_id: @tumbler.id, mechanic_id: @mechanic_1.id)
   end
 
   it 'displays infro for specific mechanic' do
@@ -31,6 +33,13 @@ RSpec.describe 'mechanic show page' do
     expect(page).to have_content(@hurler.name)
     expect(page).to have_content(@ferris.name)
     expect(page).to_not have_content(@scrambler.name)
+  end
+
+  it 'orders rides by thrill rating' do
+    visit "/mechanics/#{@mechanic_1.id}"
+
+    expect(@ferris.name).to appear_before(@hurler.name)
+    expect(@hurler.name).to appear_before(@tumbler.name)
   end
 
 end
