7a022e5bdd5d842454ea791465ab180271985fa6
Dylan Harper <dylanharper4@icloud.com>
2022-01-03 10:56:14 -0800

    Last edits

--- app/controllers/amusement_parks_controller.rb
+++ app/controllers/amusement_parks_controller.rb
@@ -3,6 +3,5 @@ class AmusementParksController < ApplicationController
   def show
     @park = AmusementPark.find(params[:id])
     @rides = @park.ordered_rides
-    # @rides = @park.rides.order(:name)
   end
 end
