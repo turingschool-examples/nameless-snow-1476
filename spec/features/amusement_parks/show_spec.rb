ff5434a2938ae6797ecab6d484cf359f19777944
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-01-03 12:03:22 -0700

    add avereage thrill rating instance method to amusement park model and to show page

--- spec/features/amusement_parks/show_spec.rb
+++ spec/features/amusement_parks/show_spec.rb
@@ -7,18 +7,44 @@ RSpec.describe "Amusement Park Show Page" do
       cedar_point = AmusementPark.create!(name: "Cedar Point", admission_cost: 58)
 
       visit "amusement_parks/#{six_flags.id}"
-      
+
       expect(page).to have_content(six_flags.name)
       expect(page).to have_content("Admissions: $65.00")
       expect(page).to_not have_content(cedar_point.name)
       expect(page).to_not have_content("Admissions: $58.00")
     end
 
-    xit 'has the names of all its rides in alphabetical order' do
-        visit "amusement_parks/#{six_flags.id}"
+    it 'has the names of all its rides in alphabetical order' do
+      six_flags = AmusementPark.create!(name: "Six Flags", admission_cost: 65)
+      cedar_point = AmusementPark.create!(name: "Cedar Point", admission_cost: 58)
+      ride_1 = six_flags.rides.create!(name: "Roller Coaster", thrill_rating: 8, open: true)
+      ride_2 = six_flags.rides.create!(name: "Giant Drop", thrill_rating: 9, open: true)
+      ride_3 = six_flags.rides.create!(name: "Scrambler", thrill_rating: 6, open: false)
+      ride_4 = cedar_point.rides.create!(name: "Wooden Coaster", thrill_rating: 7, open: false)
+
+      visit "amusement_parks/#{six_flags.id}"
+
+      expect(page).to have_content("Rides:")
+      expect(page).to have_content(ride_1.name)
+      expect(page).to have_content(ride_2.name)
+      expect(page).to have_content(ride_3.name)
+      expect(page).to_not have_content(ride_4.name)
+
+      expect(ride_2.name).to appear_before(ride_1.name)
+      expect(ride_1.name).to appear_before(ride_3.name)
+      expect(ride_3.name).to_not appear_before(ride_2.name)
+      expect(ride_3.name).to_not appear_before(ride_1.name)
     end
-    xit 'has the average thrill rating of its rides' do
-        visit "amusement_parks/#{six_flags.id}"
+
+    it 'has the average thrill rating of its rides' do
+      six_flags = AmusementPark.create!(name: "Six Flags", admission_cost: 65)
+      ride_1 = six_flags.rides.create!(name: "Roller Coaster", thrill_rating: 8, open: true)
+      ride_2 = six_flags.rides.create!(name: "Giant Drop", thrill_rating: 9, open: true)
+      ride_3 = six_flags.rides.create!(name: "Scrambler", thrill_rating: 4, open: false)
+
+      visit "amusement_parks/#{six_flags.id}"
+
+      expect(page).to have_content("Average Thrill Rating of Rides: 7.0/10")
     end
   end
 end
--- spec/models/amusement_park_spec.rb
+++ spec/models/amusement_park_spec.rb
@@ -4,4 +4,24 @@ RSpec.describe AmusementPark do
   describe 'relationships' do
     it { should have_many(:rides) }
   end
+
+  describe 'Instance Methods' do
+    it 'sorts rides alphabetically' do
+      six_flags = AmusementPark.create!(name: "Six Flags", admission_cost: 65)
+      ride_1 = six_flags.rides.create!(name: "Roller Coaster", thrill_rating: 8, open: true)
+      ride_2 = six_flags.rides.create!(name: "Giant Drop", thrill_rating: 9, open: true)
+      ride_3 = six_flags.rides.create!(name: "Scrambler", thrill_rating: 6, open: false)
+
+      expect(six_flags.rides_alphabetically).to eq([ride_2, ride_1, ride_3])
+    end
+
+    it 'can average all its rides thrill rating' do
+      six_flags = AmusementPark.create!(name: "Six Flags", admission_cost: 65)
+      ride_1 = six_flags.rides.create!(name: "Roller Coaster", thrill_rating: 8, open: true)
+      ride_2 = six_flags.rides.create!(name: "Giant Drop", thrill_rating: 9, open: true)
+      ride_3 = six_flags.rides.create!(name: "Scrambler", thrill_rating: 4, open: false)
+      
+      expect(six_flags.average_thrill_rating).to eq(7)
+    end
+  end
 end
