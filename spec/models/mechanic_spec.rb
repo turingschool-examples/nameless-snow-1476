ad9ab8fe2193c3c7cb9a8064aefc93088e0e0bc4
Paul Leonard <paulpaulleonard@gmail.com>
2022-01-03 13:20:24 -0500

    write test for model method

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -4,4 +4,13 @@ RSpec.describe Mechanic do
   describe 'relationships' do
     it { should have_many(:rides).through(:ride_mechanics) }
   end
+
+  describe "methods" do
+    it "can return the average years experience for all mechanics" do
+      mech1 = Mechanic.create(name: "Paul", years_experience: 2)
+      mech2 = Mechanic.create(name: "paulie", years_experience: 4)
+
+      expect(Mechanic.avg_years_exp).to eq 3
+    end
+  end
 end
