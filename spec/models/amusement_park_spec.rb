8e96d1d7e0e3428350d78714c218cd845e948b0a
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-01-03 11:04:30 -0700

    Rewrote test to satisfy data needs

--- spec/models/amusement_park_spec.rb
+++ spec/models/amusement_park_spec.rb
@@ -7,15 +7,13 @@ RSpec.describe AmusementPark do
 
   let!(:wonder_wharf) {AmusementPark.create!(name: "Wonder Wharf", admission_cost: 3)}
 
-  let(:merry_go_round) {Ride.create!(name: "Happy Whale", thrill_rating: 3, open: true, amusement_park: wonder_wharf)}
-  let(:coaster) {Ride.create!(name: "Whirly Whale", thrill_rating: 8, open: true, amusement_park: wonder_wharf)}
-  let(:bumper_cars) {Ride.create!(name: "Bumpy Whale", thrill_rating: 6, open: true, amusement_park: wonder_wharf)}
-  let(:haunted_house) {Ride.create!(name: "Ghost Whale", thrill_rating: 7, open: false, amusement_park: wonder_wharf)}
-  let(:skating_rink) {Ride.create!(name: "Frozen Whale", thrill_rating: 5, open: false, amusement_park: wonder_wharf)}
-
   describe '#rides_alpha' do
     it 'returns all rides in park in alphabetical order by name' do
-      expect(wonder_wharf.rides_alpha).to eq([bumper_cars, skating_rink, haunted_house, merry_go_round, coaster])
+      wonder_wharf.rides.create!(name: "Happy Whale", thrill_rating: 3, open: true)
+      wonder_wharf.rides.create!(name: "Bumpy Whale", thrill_rating: 6, open: true)
+      wonder_wharf.rides.create!(name: "Ghost Whale", thrill_rating: 7, open: false)
+
+      expect(wonder_wharf.rides_alpha).to eq(["Bumpy Whale", "Ghost Whale", "Happy Whale"])
     end
   end
 end
