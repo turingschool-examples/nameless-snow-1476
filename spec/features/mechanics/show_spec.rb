ae371ce23697e200a02b42463b4dc95d1b65bf4d
Brad Breiten <jonathan@breiten.us>
2022-01-03 11:21:10 -0700

    Test: adding ride to mechanic workload on mechanic show page

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe 'Mechanic Show Page', type: :feature do
   let!(:splash) {six_flags.rides.create!(name: "Log Ride", thrill_rating: 3, open: true)}
   let!(:coaster) {six_flags.rides.create!(name: "Cyclone", thrill_rating: 5, open: true)}
   let!(:hot_dog) {six_flags.rides.create!(name: "Hot Dog Stand", thrill_rating: 1, open: false)}
+  let!(:whirl) {six_flags.rides.create!(name: "Whirly Dirly", thrill_rating: 2, open: true)}
 
   let!(:mechanic_1) {Mechanic.create!(name: "Ted", years_of_experience: 10, rides: [splash, coaster, hot_dog])}
   let!(:mechanic_2) {Mechanic.create!(name: "Meg", years_of_experience: 3, rides: [splash])}
@@ -31,9 +32,41 @@ RSpec.describe 'Mechanic Show Page', type: :feature do
 
   describe 'form to add ride to mechanic workload' do
     it 'has a form' do
+      visit "/mechanics/#{mechanic_1.id}"
+
       expect(page).to have_content("Add Ride to Workload")
       expect(page).to have_field("Ride ID")
       expect(page).to have_button("Submit")
+    end
+
+    it 'adds a ride to the mechanic workload and displays on page after submission' do
+      visit "/mechanics/#{mechanic_1.id}"
+
+      expect(page).to have_content(splash.name)
+      expect(page).to have_content(coaster.name)
+
+      expect(page).to have_no_content(hot_dog.name)
+      expect(page).to have_no_content(whirl.name)
+
+      fill_in 'ride_id', with: whirl.id
+      click_button 'Submit'
+
+      expect(current_path).to eq("/mechanics/#{mechanic_1.id}")
+      
+      within "#rides" do
+        expect(page).to have_content(splash.name)
+        expect(page).to have_content(coaster.name)
+        expect(page).to have_content(whirl.name)
+
+        expect(page).to have_no_content(hot_dog.name)
+
+        no_1_thrilling = coaster.name
+        no_2_thrilling = splash.name
+        no_3_thrilling = whirl.name
+
+        expect(no_1_thrilling).to appear_before(no_2_thrilling, only_text: true)
+        expect(no_2_thrilling).to appear_before(no_3_thrilling, only_text: true)
+      end
     end
   end
 end
