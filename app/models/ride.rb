e40d395daca6cf3dba1dbe404d711e6de50017c9
Robin Dittrich <robinodittrich@gmail.com>
2022-01-03 08:34:56 -0800

    Complete relationships and validations set up.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,7 +1,7 @@
 class Ride < ApplicationRecord
   belongs_to :amusement_park
-  has_many :mechanic_rides
   has_many :mechanics, through: :mechanic_rides
+  has_many :mechanic_rides
 
   validates_presence_of :name, :thrill_rating, :open
 end
