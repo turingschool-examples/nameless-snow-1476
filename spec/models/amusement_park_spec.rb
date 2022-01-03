d70edbb0fe458c48a621fe626ec0dd519b7a730d
Kevin Gloss <kgloss89@gmail.com>
2022-01-03 11:27:49 -0700

    Test for amusement park rides alphabetical

--- spec/models/amusement_park_spec.rb
+++ spec/models/amusement_park_spec.rb
@@ -4,4 +4,17 @@ RSpec.describe AmusementPark do
   describe 'relationships' do
     it { should have_many(:rides) }
   end
+
+  before :each do
+    @six_flags = AmusementPark.create!(name: 'Six Flags', admission_cost: 75)
+
+    @hurler = @six_flags.rides.create!(name: 'The Hurler', thrill_rating: 5, open: true)
+    @scrambler = @six_flags.rides.create!(name: 'The Scrambler', thrill_rating: 7, open: true)
+    @wheel = @six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 7, open: false)
+    @mind = @six_flags.rides.create!(name: 'Mind Eraser', thrill_rating: 9, open: true)
+  end
+
+  it 'can sort rides alphabetically' do
+    expect(@six_flags.alphabetical).to eq([@wheel, @mind, @hurler, @scrambler])
+  end
 end
