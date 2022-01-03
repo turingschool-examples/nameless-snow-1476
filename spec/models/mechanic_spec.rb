b45b99a8e6709648347e82d422bb07ad690cfd9e
Joel Grant <joelmgrant@me.com>
2022-01-03 10:19:11 -0700

    Test: Add model test for average experience

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -1,8 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Mechanic do
+  before(:each) do
+    @mechanic_1 = Mechanic.create(name: "Bob", years_experience: 10)
+    @mechanic_2 = Mechanic.create(name: "Pam", years_experience: 14)
+    @mechanic_3 = Mechanic.create(name: "Michael", years_experience: 3)
+  end
   describe 'relationships' do
     it { should have_many(:ride_mechanics) }
     it { should have_many(:rides).through(:ride_mechanics)}
+  end
+
+  describe '::experience' do
+    it 'returns the average years of experience of all mechanics' do
+      expect(Mechanic.experience).to eq(9)
+    end
   end
 end
