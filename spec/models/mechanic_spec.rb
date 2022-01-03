45e137b53cf49db186917e901fc033ac10cf95a4
Robin Dittrich <robinodittrich@gmail.com>
2022-01-03 09:15:31 -0800

    Create test for yoe_average.

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -9,4 +9,16 @@ RSpec.describe Mechanic do
     it { should validate_presence_of :name }
     it { should validate_presence_of :years_experience }
   end
+
+  describe 'class methods' do
+    before(:each) do
+      @mechanic_1 = Mechanic.create!(name: "Robin", years_experience: 12)
+      @mechanic_2 = Mechanic.create!(name: "Daisy", years_experience: 20)
+      @mechanic_3 = Mechanic.create!(name: "Richard", years_experience: 4)
+    end
+
+    it '#yoe_average' do #Years of Experience
+      expect(Mechanic.yoe_average).to eq(12)
+    end
+  end
 end
