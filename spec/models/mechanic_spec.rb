c4cae2582387ba08b21b4b602197148ec5be1419
Karan Mehta <karanm645@gmail.com>
2022-01-03 10:40:50 -0600

    Create and implement average experience AR method

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -5,4 +5,15 @@ RSpec.describe Mechanic do
     it { should have_many :mechanic_rides }
     it { should have_many(:rides).through(:mechanic_rides) }
   end
+
+  before :each do 
+    @mechanic_1 = Mechanic.create!(name: "Mechanic 1", experience: 12)
+    @mechanic_2 = Mechanic.create!(name: "Mechanic 2", experience: 15)
+  end 
+
+  describe '#class_methods' do 
+    it 'average_experience' do 
+      expect(Mechanic.average_experience).to eq(13.5)
+    end 
+  end 
 end
