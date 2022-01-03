5900466c07d7e1f6cb1f465ed1d3ba638e95a401
Jessica Grazulis <jlgrazulis@gmail.com>
2022-01-03 11:26:04 -0700

    Add mechanic show spec

---
+++ spec/features/mechanics/show_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+# As a user,
+# When I visit a mechanic show page
+# I see their name, years of experience, and the names of rides they’re working on
+# And I only see rides that are open
+# And the rides are listed by thrill rating in descending order (most thrills first)
+
+RSpec.describe 'mechanic show' do
+  it 'shows the mechanics attributes and open rides theyre working on' do
+    mechanic = Mechanic.create!(name: "Sally Field", years_experience: 15)
+
+    visit "/mechanics/#{mechanic.id}"
+
+    expect(page).to have_content(mechanic.name)
+    expect(page).to have_content(mechanic.years_experience)
+    expect(page).to have_content(mechanic.rides)
+
+    
+  end
+end
