0f3cd5530c7bc5d1aab17dab7f639435a1505164
Parker <88950699+ParkerLockhart@users.noreply.github.com>
2022-01-03 11:07:05 -0600

    Average_experience class method test

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -5,4 +5,18 @@ RSpec.describe Mechanic do
     it { should have_many(:ride_mechanics) }
     it { should have_many(:rides).through(:ride_mechanics) }
   end
-end 
+
+  describe 'class methods' do
+    before(:each) do
+      @mechanic = Mechanic.create!(name: "Kara Smith", years_experience: 11)
+      @mechanic2 = Mechanic.create!(name: "Jeff Burton", years_experience: 8)
+      @mechanic3 = Mechanic.create(name: "Taylor Jennings", years_experience: 3)
+    end
+
+    describe 'average_experience' do
+      it 'averages years_experience for all mechanics' do
+        expect(Mechanic.average_experience).to eq(7)
+      end
+    end 
+  end
+end
