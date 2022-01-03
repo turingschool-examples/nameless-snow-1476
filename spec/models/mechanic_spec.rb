895e4febc40372dd8115e6dcc9c13f8874d6c916
Kelly Anderson <kean9528@colorado.edu>
2022-01-03 09:29:43 -0700

    test all relationships

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Ride do
+RSpec.describe Mechanic do
   describe 'relationships' do
-    it { should belong_to(:amusement_park) }
-    it { should have_many(:mechanics) }
+    it { should have_many(:rides) }
     it { should have_many(:ride_mechanics) }
   end
 end
