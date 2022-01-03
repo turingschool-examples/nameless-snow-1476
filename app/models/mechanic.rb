33b7cd812bc0ace7d3f1cadb6ebd424099f792fe
Hannah Warren <hannahkwarren@gmail.com>
2022-01-03 11:46:06 -0500

    Completes user story 1 - mechanics:rides relationship and mechanics index page

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,4 +1,8 @@
 class Mechanic < ApplicationRecord 
   has_many :mechanic_rides
   has_many :rides, through: :mechanic_rides
+
+  def self.avg_years_experience
+    average(:years_experience).round
+  end
 end
--- spec/models/mechanic_ride_spec.rb
+++ spec/models/mechanic_ride_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe MechanicRides, type: :model do 
+RSpec.describe MechanicRide, type: :model do 
 
   describe "relationships" do 
     it { should belong_to(:mechanic) }
