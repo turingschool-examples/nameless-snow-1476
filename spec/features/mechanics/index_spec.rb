6f6b5785159781fc596fe2a48786ac07991b1353
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-01-03 09:32:43 -0700

    Tests for user story one actually written now:

--- spec/features/mechanics/index_spec.rb
+++ spec/features/mechanics/index_spec.rb
@@ -7,11 +7,28 @@ RSpec.describe 'mechanic index page' do
   let!(:jim) {Mechanic.create!(name: "Jim Redman", years_of_experience: 35)}
 
   it "has a header that says 'All Mechanics' " do
+
+    visit '/mechanics'
+
+    expect(page).to have_content("All Mechanics")
   end
 
   it 'lists all mechanics names and years of experience' do
+
+    visit '/mechanics'
+
+    expect(page).to have_content(teddy.name)
+    expect(page).to have_content(teddy.years_of_experience)
+    expect(page).to have_content(bob.name)
+    expect(page).to have_content(bob.years_of_experience)
+    expect(page).to have_content(jim.name)
+    expect(page).to have_content(jim.years_of_experience)
   end
 
   it 'displays the average years of experience for all mechanics' do
+
+    visit '/mechanics'
+
+    expect(page).to have_content("Average Years of Experience: #{Mechanic.average_exp}")
   end
 end
