ca405f0e42a8c01bedc2e65ccb4d09e63538c208
Leland Curtis <lelandcurtis88@gmail.com>
2022-01-03 11:51:06 -0500

    add edge case tets to avg_years_experience.

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -3,13 +3,18 @@ require 'rails_helper'
 RSpec.describe Mechanic, type: :model do
   describe 'class methods' do
     describe 'avg_years_experience' do
-      it 'returns the avg_years_experience of all mechanics' do
+      it 'returns the avg_years_experience of all mechanics if mechanics exist' do
         mech_1 = create(:mechanic, years_experience: 10)
         mech_1 = create(:mechanic, years_experience: 15)
         mech_1 = create(:mechanic, years_experience: 13)
 
         expect(Mechanic.avg_years_experience).to eq(12.67)
       end
+
+      it 'returns nil if no mechanics exist' do
+        expect(Mechanic.avg_years_experience).to eq(nil)
+      end
+
     end
   end
 end
