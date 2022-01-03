463e163076853cc042b5e0f04dc01b14bd255c75
Kevin Gloss <kgloss89@gmail.com>
2022-01-03 09:59:14 -0700

    Test average experience method

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe Mechanic do
     it { should have_many(:mechanic_rides) }
     it { should have_many(:rides).through(:mechanic_rides) }
   end
+
+  before :each do
+    @greg = Mechanic.create!(name: "Greg", years_experience: 6)
+    @mary = Mechanic.create!(name: "Mary", years_experience: 8)
+    @jones = Mechanic.create!(name: "Jones", years_experience: 16)
+  end
+
+  describe 'class methods' do
+    it 'can calculate the average experience of mechanics' do
+      expect(Mechanic.average_experience).to eq(10)
+    end
+  end
 end
