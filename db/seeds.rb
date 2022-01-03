43db482c18bc253c99beb1f618500ff64da737f9
Kevin Gloss <kgloss89@gmail.com>
2022-01-03 11:50:33 -0700

    Add mechanics and mechanic/ride objects to test local

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,16 @@ universal = AmusementPark.create!(name: 'Universal Studios', admission_cost: 80)
 hurler = six_flags.rides.create!(name: 'The Hurler', thrill_rating: 7, open: true)
 scrambler = six_flags.rides.create!(name: 'The Scrambler', thrill_rating: 4, open: true)
 wheel = six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 7, open: false)
+mind = six_flags.rides.create!(name: 'Mind Eraser', thrill_rating: 9, open: true)
 
 jaws = universal.rides.create!(name: 'Jaws', thrill_rating: 5, open: true)
+
+greg = Mechanic.create!(name: "Greg", years_experience: 6)
+mary = Mechanic.create!(name: "Mary", years_experience: 8)
+jones = Mechanic.create!(name: "Jones", years_experience: 16)
+
+gh = MechanicRide.create!(mechanic: greg, ride: hurler)
+gs = MechanicRide.create!(mechanic: greg, ride: scrambler)
+gw = MechanicRide.create!(mechanic: greg, ride: wheel)
+mh = MechanicRide.create!(mechanic: mary, ride: hurler)
+js = MechanicRide.create!(mechanic: jones, ride: scrambler)
