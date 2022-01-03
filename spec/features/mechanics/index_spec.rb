fa573cdeba44dc86e56484f9b8af73daa2b03e96
Paul Leonard <paulpaulleonard@gmail.com>
2022-01-03 12:42:13 -0500

    forked and pushed since i didnt fork before

--- spec/features/mechanics/index_spec.rb
+++ spec/features/mechanics/index_spec.rb
@@ -1,19 +1,19 @@
-# require 'rails_helper'
-#
-# RSpec.describe 'Mechanics Index' do
-#   xit "has a header" do
-#     visit '/mechanics'
-#
-#     expect(page).to have_content("All Mechanics")
-#   end
-#
-#   xit "lists all mechanics by name and has their years of experience" do
-#     mech1 = Mechanic.create(name: "Paul", :years_experience: 2)
-#     mech2 = Mechanic.create(name: "paulie", :years_experience: 4)
-#
-#     visit '/mechanics'
-#
-#
-#   end
-#
-# end
+require 'rails_helper'
+
+RSpec.describe 'Mechanics Index' do
+  xit "has a header" do
+    visit '/mechanics'
+
+    expect(page).to have_content("All Mechanics")
+  end
+
+  xit "lists all mechanics by name and has their years of experience" do
+    mech1 = Mechanic.create(name: "Paul", years_experience: 2)
+    mech2 = Mechanic.create(name: "paulie", years_experience: 4)
+
+    visit '/mechanics'
+
+
+  end
+
+end
