1be0907f70a7748a74b81e988f9c10fdaadf69fe
Leland Curtis <lelandcurtis88@gmail.com>
2022-01-03 12:44:18 -0500

    Write test for by_thrill.

--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -6,4 +6,16 @@ RSpec.describe Ride do
     it { should have_many(:ride_mechanics)}
     it { should have_many(:mechanics).through(:ride_mechanics)}
   end
+
+  describe 'class methods' do
+    describe 'by_thrill' do
+      it 'returns rides in descending order by thrill rating' do
+        ride_1 = create(:ride, thrill_rating: 10)
+        ride_2 = create(:ride, thrill_rating: 15)
+        ride_3 = create(:ride, thrill_rating: 4)
+
+        expect(Ride.by_thrill).to eq([ride_2, ride_1, ride_3])
+      end
+    end
+  end
 end
