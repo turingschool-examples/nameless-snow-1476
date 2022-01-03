2280406176df76d54e62b8f9793685564853ae1c
Leland Curtis <lelandcurtis88@gmail.com>
2022-01-03 11:29:29 -0500

    Write feature tests for user story 1 mech index page.

---
+++ spec/features/mechanics/index_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe 'mechanics show page' do
+  it 'has a header called "All Mechanics"' do
+    visit '/mechanics'
+    expect(page).to have_content("All Mechanics")
+  end
+
+  it "lists all mechanics" do
+    mech_1 = create(:mechanic, name: 'Bob')
+    mech_2 = create(:mechanic, name: 'Terry')
+
+    visit '/mechanics'
+
+    expect(page).to have_content('Bob')
+    expect(page).to have_content('Terry')
+  end
+
+  it "lists the age of all mechanics" do
+    mech_1 = create(:mechanic, age: 10)
+    mech_2 = create(:mechanic, age: 12)
+
+    visit '/mechanics'
+
+    expect(page).to have_content(10)
+    expect(page).to have_content(12)
+  end
+
+  it 'lists the average years of experience across all mechanics' do
+    mech_1 = create(:mechanic, years_experience: 5)
+    mech_2 = create(:mechanic, years_experience: 3)
+    mech_3 = create(:mechanic, years_experience: 10)
+
+    visit '/mechanics'
+
+    expect(page).to have_content(Mechanic.avg_years_experience)
+  end
+end
