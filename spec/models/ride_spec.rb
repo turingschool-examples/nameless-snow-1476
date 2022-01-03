1afe53906eb1f65b3b197400a5a97ce2de90c890
Kevin Gloss <kgloss89@gmail.com>
2022-01-03 10:28:37 -0700

    Test desc thrill and rides open methods

--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -6,4 +6,20 @@ RSpec.describe Ride do
     it { should have_many(:mechanic_rides) }
     it { should have_many(:mechanics).through(:mechanic_rides) }
   end
+
+  before :each do
+    @six_flags = AmusementPark.create!(name: 'Six Flags', admission_cost: 75)
+
+    @hurler = @six_flags.rides.create!(name: 'The Hurler', thrill_rating: 5, open: true)
+    @scrambler = @six_flags.rides.create!(name: 'The Scrambler', thrill_rating: 7, open: true)
+    @wheel = @six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 6, open: false)
+  end
+
+  it 'sorts to only open rides' do
+    expect(Ride.open?).to eq([@hurler, @scrambler])
+  end
+
+  it 'sorts the rides by desc thrill rating' do
+    expect(Ride.desc_thrill).to eq([@scrambler, @wheel, @hurler])
+  end
 end
