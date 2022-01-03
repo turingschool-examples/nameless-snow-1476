e5c8a1d48b21298acffa338f9adf64e21e9ceee7
Tim Roeder <timothyjroeder@gmail.com>
2022-01-03 09:36:47 -0700

    Feat: add Mechanic validations and tests

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe Mechanic, type: :model do
     it { should have_many(:ride_mechanics) }
     it { should have_many(:rides).through(:ride_mechanics) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :years_experience }
+  end
 end
