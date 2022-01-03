e30074e48d890c377d44ad753d3605b7c2412ada
Paul Leonard <paulpaulleonard@gmail.com>
2022-01-03 13:31:49 -0500

    open_rides method tested and functional

--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -5,4 +5,17 @@ RSpec.describe Ride do
     it { should belong_to(:amusement_park) }
     it { should have_many(:mechanics).through(:ride_mechanics) }
   end
+
+  describe 'model methods' do
+    it "can list only rides that are open" do
+      park = AmusementPark.create!(name: "universal", admission_cost: 100)
+      ride1 = park.rides.create!(name: "the hulk", thrill_rating: 10, open: false)
+      ride2 = park.rides.create!(name: "space mountain", thrill_rating: 3, open: true)
+      ride3 = park.rides.create!(name: "E.T.", thrill_rating: 1, open: true)
+      rides = [ride1, ride2, ride3]
+
+      expect(Ride.open_rides).to eq([ride2, ride3])
+
+    end
+  end
 end
