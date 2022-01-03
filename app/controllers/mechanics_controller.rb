4ddbcba61a076616b02646437ddc9465e1c059d2
Seth Perna <gseth26@gmail.com>
2022-01-03 13:50:54 -0500

    finish user story 3 with feature tests, simplecov at 100% on models and features

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -10,7 +10,7 @@ class MechanicsController < ApplicationController
   def update
     mechanic = Mechanic.find(params[:id])
     ride = Ride.find(params[:ride_id])
-    @mechanic_ride = MechanicRide.create!(mechanic_id: mechanic.id, ride_id: ride.id)
+    mechanic_ride = MechanicRide.create!(mechanic_id: mechanic.id, ride_id: ride.id)
     redirect_to "/mechanics/#{mechanic.id}"
   end
 end
