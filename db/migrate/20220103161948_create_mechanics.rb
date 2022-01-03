fed7ba06e4e38df5ad4e4aa559e0e3bb980c60eb
Karan Mehta <karanm645@gmail.com>
2022-01-03 10:21:10 -0600

    Migrate Mechanics

---
+++ db/migrate/20220103161948_create_mechanics.rb
@@ -0,0 +1,10 @@
+class CreateMechanics < ActiveRecord::Migration[5.2]
+  def change
+    create_table :mechanics do |t|
+      t.string :name
+      t.integer :experience
+
+      t.timestamps
+    end
+  end
+end
