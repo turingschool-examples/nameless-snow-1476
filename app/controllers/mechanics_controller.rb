dbd6d779127c5c8383787672100cfeb98925ff8d
Leland Curtis <lelandcurtis88@gmail.com>
2022-01-03 11:56:44 -0500

    Add status to mech controller index to return text if no mechanic exists.

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -2,6 +2,12 @@ class MechanicsController < ApplicationController
 
   def index
     @mechanics = Mechanic.all
+
+    if Mechanic.avg_years_experience
+      @status = Mechanic.avg_years_experience
+    else
+      @status = "There are no Mechanics"
+    end
   end
 
 end
