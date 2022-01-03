72349c4b642658263cee89e96d238ab62bd19968
Tim Roeder <timothyjroeder@gmail.com>
2022-01-03 11:56:35 -0700

    Feat: add amusement_parks #show resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :mechanics, only: [:index, :show, :update]
+  resources :amusement_parks, only: [:show]
 end
