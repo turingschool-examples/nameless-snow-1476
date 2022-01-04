59f0bc2ed845ed9db828dd07538cb1852702c05d
David Kassin <david.j.kassin@gmail.com>
2022-01-03 17:59:42 -0700

    Add story 1 test and working code

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,4 +1,9 @@
 class Mechanic < ApplicationRecord
   has_many :mechanic_rides
   has_many :rides, through: :mechanic_rides
+
+  def self.avg_age
+    average(:years_experience).round(1)
+  end
+
 end
--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -5,4 +5,13 @@ RSpec.describe Mechanic, type: :model do
     it { should have_many :mechanic_rides}
     it {should have_many(:rides).through(:mechanic_rides)}
   end
+
+  it 'finds the average age of all mechanics' do
+    mech_1 = Mechanic.create!(name: "Jon", years_experience: 12)
+    mech_2 = Mechanic.create!(name: "James", years_experience: 14)
+    mech_3 = Mechanic.create!(name: "Sam", years_experience: 9)
+    mech_4 = Mechanic.create!(name: "Mike", years_experience: 11)
+
+    expect(Mechanic.avg_age).to eq(11.5)
+  end
 end
