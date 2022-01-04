94ecad82d0a4c2d7ef00d0792df2e6625a2423f9
Eric Chon <echon92@gmail.com>
2022-01-03 18:24:49 -0700

    models test passing for joins table

--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Ride do
+RSpec.describe Ride, type: :model do
   describe 'relationships' do
     it { should belong_to(:amusement_park) }
     it { should have_many(:mechanic_rides)}
